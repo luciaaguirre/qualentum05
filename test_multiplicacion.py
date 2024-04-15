cb27d4846f82cd5bf81845e3dc0f8185ca8e1dca
lucia aguirre <guerrolucia@gmail.com>
2024-04-15 15:51:50 +0200

    inclusion multiplicaciones

---
+++ test_multiplicacion.py
@@ -0,0 +1,11 @@
+import unittest
+from multiplicacion import multiplicar
+
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(multiplicar(3,2), 6)
+        self.assertEqual(multiplicar(2,1), 2)
+        self.assertEqual(multiplicar(2, 5), 10)
+
+if __name__ == "__main__":
+    unittest.main()
